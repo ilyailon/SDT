2a90ec13cf28e37e18d131aff3d474e2eca13773
Sunrise <sunriseqq99@gmail.com>
2019-10-01 22:36:37 +0300

    changeMessageVC

--- myChat/myChat/Controller/MessageVC.swift
+++ myChat/myChat/Controller/MessageVC.swift
@@ -1,1 +1,96 @@
+//
+//  MessageVC.swift
+//  myChat
+//
+//  Created by Sunrise on 23/04/2019.
+//  Copyright © 2019 Sunrise. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseDatabase
+import SwiftKeychainWrapper
+
+class MessageVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextViewDelegate {
+    
+    @IBOutlet weak var sendButton: UIButton!
+    
+    @IBOutlet weak var messageField: UITextField!
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var messages = [Message]()
+    
+    var message: Message!
+    
+    var currentUser = KeychainWrapper.standard.string(forKey: "uid")
+    
+    var messageId: String!
+    
+    var recipient: String!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        
+        tableView.dataSource = self
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        tableView.estimatedRowHeight = 300
+        
+        if messageId != "" && messageId != nil {
+            loadData()
+        }
+        // Do any additional setup after loading the view.
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let message = messages[indexPath.row]
+        
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "Message") as? MessagesCell {
+            
+            cell.configCell(message: message)
+            
+            return cell
+        } else {
+            return MessagesCell()
+        }
+    }
+    func loadData() {
+        Database.database().reference().child("messages").child(messageId).observe(.value, with: { (snapshot) in
+            
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                
+                self.messages.removeAll()
+                
+                for data in snapshot {
+                    
+                    if let postDict = data.value as? Dictionary<String, AnyObject> {
+                        
+                        let key = data.key
+                        
+                        let post = Message(messageKey: key, postData: postDict)
+                        
+                        self.messages.append(post)
+                    }
+                    
+                }
+                
+            }
+            
+        })
+    }
+}
 
