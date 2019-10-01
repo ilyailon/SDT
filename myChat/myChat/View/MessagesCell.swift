47f0e9884000849f71a9536160f586e3c828fe7f
Sunrise <sunriseqq99@gmail.com>
2019-10-01 22:30:38 +0300

    changeMessageCell

--- myChat/myChat/View/MessagesCell.swift
+++ myChat/myChat/View/MessagesCell.swift
@@ -1,1 +1,63 @@
+//
+//  MessagesCell.swift
+//  myChat
+//
+//  Created by Sunrise on 24/04/2019.
+//  Copyright © 2019 Sunrise. All rights reserved.
+//
+
+import UIKit
+import SwiftKeychainWrapper
+
+class MessagesCell: UITableViewCell {
+    
+    @IBOutlet weak var recievedMessageLbl: UILabel!
+    
+    @IBOutlet weak var recievedMessageView: UIView!
+    
+    @IBOutlet weak var sentMessageLbl: UILabel!
+    
+    @IBOutlet weak var sentMessageView: UIView!
+    
+    var message: Message!
+    
+    var currentUser = KeychainWrapper.standard.string(forKey: "uid")
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    func configCell(message: Message) {
+        self.message = message
+        
+        if message.sender == currentUser {
+            
+            sentMessageView.isHidden = false
+            
+            sentMessageLbl.text = message.message
+            
+            recievedMessageLbl.text = ""
+            
+            recievedMessageLbl.isHidden = true
+        } else {
+            
+            sentMessageView.isHidden = true
+            
+            sentMessageLbl.text = ""
+            
+            recievedMessageLbl.text =  message.message
+            
+            recievedMessageLbl.isHidden = false
+            
+            
+        }
+        
+    }
+}
 
